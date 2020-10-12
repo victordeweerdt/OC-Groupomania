143b22ab0f04878e14ca0b82101cba3f2186be32
Victor Deweerdt <deweerdt.victor@gmail.com>
2020-10-12 17:41:12 +0200

    Initialisation

---
+++ api-keeplink/app.js
@@ -0,0 +1,15 @@
+// Imports
+const express = require('express');
+
+// Init serveur
+const app = express();
+
+// Configure routes
+app.use('/', function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
